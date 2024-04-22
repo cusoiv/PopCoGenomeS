858bec8b1336ed402b0f63bb73e4bd7aadb8ada5
cusoiv <cusoiv@gmail.com>
2024-04-22 12:48:29 +0200

    Update phybreak_config.sh

--- src/PopCoGenomeS_part_2/phybreak_config.sh
+++ src/PopCoGenomeS_part_2/phybreak_config.sh
@@ -1,9 +1,8 @@
-#define project_dir and file basename, need to be the same as that in phybreak_parameters.txt
 output_dir='/lisc/scratch/dome/xy43/UHGG_plus4/Results/PopcoGenomeS'  #this is the folder where you want your outputs to be 
-project_dir_1='/lisc/scratch/dome/xy43/UHGG_plus4/Scripts/tmp'	#where is your project workspace))
-basename='anicluster90diffH_UHGG_BN10_GMC'   #this should be the same as the previous basename in the first part of PopCOGenomeS
+project_dir_1='/lisc/scratch/dome/xy43/UHGG_plus4/Scripts/tmp'	#where is your project workspace
+basename='example'   #this should be the same as the previous basename in the first part of PopCoGenomeS
 genome_source='/lisc/scratch/dome/xy43/UHGG_plus4/Data/genomes' #the folder where your genomes are in
-genome_ext='.fna.gz'
+genome_ext='.fna.gz' #can be .fna.gz, .fna, .fasta, .fasta.gz
 pop_infile_source='/lisc/scratch/dome/xy43/UHGG_plus4/Results/PopcoGenomeS'  #where are your genome lists for each vertically-inherited cluster identified in step 1
 path_to_phybreak='/lisc/scratch/dome/xy43/UHGG_plus4/Scripts' #where is your align_and_construct_trees folder
 path_to_PopCoGenomeS='/lisc/user/yu/envs/popcogenomes' #path to your conda environment PopCoGenomeS
