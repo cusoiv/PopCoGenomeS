6cf6cc788ef528beaa8b32807ca5bc6783dc4dce
cusoiv <cusoiv@gmail.com>
2024-04-27 21:21:11 +0200

    Update fasta_to_phy.R

--- src/PopCoGenomeS_part_2/align_and_construct_trees/fasta_to_phy.R
+++ src/PopCoGenomeS_part_2/align_and_construct_trees/fasta_to_phy.R
@@ -1,4 +1,4 @@
-library(phylotools, lib.loc="~/Rlibs_4")
+library(phylotools)
 args = commandArgs(trailingOnly=TRUE)
 fasta=read.fasta(file = paste0("../align/",args[1],".core.fasta"))
 dat2phylip(fasta, outfile = paste0("../align/",args[1],".core.fasta.phy"))
